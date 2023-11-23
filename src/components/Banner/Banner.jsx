d927ab67ad61c4aae638d349f3fb1d257aa1c5f3
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-11-23 10:55:35 -0300

    responsivo da home feito

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
 import imgBanner from "../../../public/assets/banner2.png"
+import imgBanner2 from "../../../public/assets/bannerPrincipal.png"
 import "./Banner.scss"
+import { useEffect } from "react"
 function Banner() {
+    const [largura, setLargura] = useState(window.innerWidth)
+    const [srcImagem, setSrcImagem] = useState("")
+
+    useEffect(()=>{
+        const handleResize = () =>{
+            setLargura(window.innerWidth)
+        }
+
+        window.addEventListener("resize", handleResize)
+        return () =>{
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
+
+    useEffect(()=>{
+        if(largura < 800){
+            setSrcImagem(imgBanner2)
+        }else{
+            setSrcImagem(imgBanner)
+        }
+    }, [largura])
+
     return (
         <>     <div className="divContainer">
-                    <img className="imagemBanner slide-in-right" src={imgBanner} style={{width:"100%"}} alt="" />
+                    <img className="imagemBanner slide-in-right" src={srcImagem} style={{width:"100%"}} alt="" />
                     <h1 className="text-focus-in">Inovação em Hidratação para uma vida melhor</h1>
                     <p className="text-focus-in">A Fluence é uma startup com o foco em cuidado especializado, usando tecnologia no controle da <span>hidratação de doentes & idosos</span></p>
                     <button className="slide-in-left">Saiba mais</button>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,29 +3,29 @@ import sociais from "../../../public/assets/sociais.png";
 function Footer() {
   return (
     <footer>
-
       <div className="divSociais">
         <p>Fluence</p>
         <img src={sociais} alt="" />
       </div>
+      <div className="divUls">
+        <div className="divInfos">
+          <p>Links</p>
+          <ul className="ulFooter">
+            <li>Home</li>
+            <li>Solução</li>
+            <li>Sobre nós</li>
+            <li>Serviços</li>
+          </ul>
+        </div>
 
-      <div className="divInfos">
-        <p>Links</p>
-        <ul className="ulFooter">
-          <li>Home</li>
-          <li>Solução</li>
-          <li>Sobre nós</li>
-          <li>Serviços</li>
-        </ul>
-      </div>
-
-      <div className="divInfos">
-        <p>Suporte</p>
-        <ul className="ulFooter">
+        <div className="divInfos">
+          <p>Suporte</p>
+          <ul className="ulFooter">
             <li>FAQ</li>
             <li>Como funciona</li>
             <li>Contato</li>
-        </ul>
+          </ul>
+        </div>
       </div>
     </footer>
   );
